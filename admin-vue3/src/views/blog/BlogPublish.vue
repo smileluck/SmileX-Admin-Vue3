50a30debf3d54842c5be708104c8e2f10b46e462
smileluck <smilex@zsmile.top>
2022-07-05 10:28:55 +0800

    update md editor

--- admin-vue3/src/views/blog/BlogPublish.vue
+++ admin-vue3/src/views/blog/BlogPublish.vue
@@ -1,33 +1,47 @@
 <template>
-  <v-md-editor
-    v-model="text"
-    height="600px"
-    ref="markdownEditor"
-    default-show-toc
-    :include-level="[1, 2, 3]"
-    :disabled-menus="[]"
-    @save="savehandler"
-    @upload-image="updateImgHandler"
-  ></v-md-editor>
+  <div ref="editorRef"></div>
 </template>
 
  <script setup>
-import { ref } from "vue";
-const text = ref("");
-const markdownEditor = ref();
+// 组件：vditor
+// https://ld246.com/article/1549638745630#id
+import { onMounted, ref, nextTick } from "vue";
+import Vditor from "vditor";
+import "vditor/dist/index.css";
 
-const savehandler = (text, html) => {
-  console.log(text, html);
-};
+const editorRef = ref();
+const content = ref("");
 
-const updateImgHandler = (event, insertImage, files) => {
-  console.log(event, insertImage, files);
-  // 此处只做示例
-  insertImage({
-    url: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1269952892,3525182336&fm=26&gp=0.jpg",
-    desc: "七龙珠",
-    // width: 'auto',
-    // height: 'auto',
+let instance;
+const init = () => {
+  instance = new Vditor(editorRef.value, {
+    height: 720,
+    mode: "sv",
+    toolbarConfig: {
+      pin: false,
+    },
+    outline: {
+      enable: true,
+    },
+    preview: {
+      delay: 500,
+      markdown: {
+        toc: true,
+      },
+    },
+    cache: {
+      enable: false,
+    },
+    after: () => {
+      instance.setValue(content.value);
+    },
+    // 这里写上传
+    upload: {},
   });
 };
+onMounted(() => {
+  nextTick(() => {
+    init();
+  });
+});
 </script>
