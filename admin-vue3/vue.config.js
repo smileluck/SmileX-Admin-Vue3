4acfc8952d349ffc9965606fa06c528dbb8f6c50
smileluck <smilex@zsmile.top>
2022-11-04 16:55:47 +0800

    打包优化

--- admin-vue3/vue.config.js
+++ admin-vue3/vue.config.js
@@ -3,12 +3,19 @@ const Components = require("unplugin-vue-components/webpack");
 const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
 const path = require("path");
 
+// 生产优化
+// 代码压缩
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
+
 // 是否是生产环境
 const isProduction = process.env.NODE_ENV === "production"
 
+
 module.exports = {
   // 设置根路径与服务器nginx部署路径有关
-  publicPath: isProduction ? "/admin" : "/",
+  publicPath: isProduction ? "./admin" : "/",
 
   // 优化1 减少打包体积，不生成map文件
   productionSourceMap: isProduction ? false : true,
@@ -43,8 +50,41 @@ module.exports = {
         resolvers: [ElementPlusResolver({ importStyle: "css" })],
         dts: "components.d.ts",
       }))
-
+    if (isProduction) {
+      // 代码压缩
+      config.plugins.push(
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            //生产环境自动删除console
+            warnings: false, // 若打包错误，则注释这行
+            compress: {
+              drop_debugger: true,
+              drop_console: true,
+              pure_funcs: ['console.log']
+            }
+          },
+          sourceMap: false,
+          parallel: true
+        })
+      )
+      // GZIP压缩
+      // const productionGzipExtensions = ['html', 'js', 'css']
+      // config.plugins.push(
+      //   new CompressionWebpackPlugin({
+      //     filename: '[path].gz[query]',
+      //     algorithm: 'gzip',
+      //     test: new RegExp(
+      //       '\\.(' + productionGzipExtensions.join('|') + ')$'
+      //     ),
+      //     threshold: 10240, // 只有大小大于该值的资源会被处理 10240
+      //     minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+      //     deleteOriginalAssets: true // 不删除原文件
+      //   })
+      // )
+    }
   },
+  // eslint开启
+  lintOnSave: !isProduction,
   // 开发配置
   devServer: {
     port: 3000,
